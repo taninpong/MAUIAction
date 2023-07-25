afb039c5b1fe9c7e9a2b1e9bd1529660f5053ab0
taninpong <thaninpong@perfenterprise.com>
2023-07-25 15:16:28 +0700

    Create Proj

---
+++ MAUIAction/MAUIAction/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MAUIAction;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MAUIAction/MAUIAction/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MAUIAction;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
